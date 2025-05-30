b8f9504add01c6a479be62fe1be98ebd759102df
James Stine <james.stine@okstate.edu>
2025-05-29 22:04:44 -0500

    update values

--- SV/hw1/prob1.sv
+++
@@ -1,6 +0,0 @@
-module sample (input logic [63:0] a, b, c, output logic [127:0] z, output logic signed [127:0] z2);
-
-   assign z = a*b+c;
-   assign z2 = $signed(a)*$signed(b) + $signed(c);
-
-endmodule // sample
